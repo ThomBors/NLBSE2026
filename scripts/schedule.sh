5425a5498bbc7cef5c52834beef6d0f0bb6a8cfc
Thomas Borsani <tborsani@unibz.it>
2025-09-03 15:33:45 +0200

    setup hydra

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -2,6 +2,5 @@
 # Schedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-python src/train.py trainer.max_epochs=5 logger=csv
+python src/main.py trainer.SYNQ=0.1
 
-python src/train.py trainer.max_epochs=10 logger=csv
