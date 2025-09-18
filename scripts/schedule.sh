2ddc14df80035a5c9e8129584f572d1b3f9a2adf
Thomas Borsani <tborsani@unibz.it>
2025-09-18 18:24:47 +0200

    some updates

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -2,5 +2,5 @@
 # Schedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-python src/main.py experiment.trainer.SYNQ=0.1
+python src/main.py experiment.trainer.SYNQ=0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9
 
